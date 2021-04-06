c51fff56ce746fb9d91cdfc1ee13a6e64ccc0d6f
Luke Kaalim <lkaalim@nine.com.au>
2021-04-06 18:31:10 +1000

    Update primitives to throw errors with text

--- primitives.js
+++ primitives.js
@@ -2,31 +2,31 @@
 
 const toObject = (value/*: mixed*/)/*: { +[string]: mixed }*/ => {
   if (typeof value !== 'object')
-    throw new TypeError();
+    throw new TypeError(`${typeof value} was not an object`);
   if (value === null)
-    throw new TypeError();
+    throw new TypeError(`${typeof value} was null`);
   return value;
 };
 
 const toArray = (value/*: mixed*/)/*: $ReadOnlyArray<mixed>*/ => {
   if (!Array.isArray(value))
-    throw new TypeError();
+    throw new TypeError(`${typeof value} was not an array`);
   return value;
 };
 
 const toNumber = (value/*: mixed*/)/*: number*/ => {
   if (typeof value !== 'number')
-    throw new TypeError();
+    throw new TypeError(`${typeof value} was not a number`);
   return value;
 };
 const toString = (value/*: mixed*/)/*: string*/ => {
   if (typeof value !== 'string')
-    throw new TypeError();
+    throw new TypeError(`${typeof value} was not a string`);
   return value;
 };
 const toBoolean = (value/*: mixed*/)/*: boolean*/ => {
   if (typeof value !== 'boolean')
-    throw new TypeError();
+    throw new TypeError(`${typeof value} was not a boolean`);
   return value;
 };
 
